9c899d2d9d26fef494a7d5fe08298989789e84f0
Trev241 <trevisliu@gmail.com>
2022-12-07 01:32:53 +0530

    Tracks can be skipped and removed from queue

--- music.py
+++ music.py
@@ -1,6 +1,7 @@
 import discord
 import youtube_dl
 import asyncio
+import datetime
 
 from discord.ext import commands
 from collections import deque
@@ -19,8 +20,6 @@ IDLE_TIMEOUT_INTERVAL = 3
 class Music(commands.Cog):
     def __init__(self, client):
         self.client = client
-
-        self.idle = True
         self.looping = False
         self.current_track = None
         self.queue = deque()
@@ -28,7 +27,7 @@ class Music(commands.Cog):
     @commands.command()
     async def join(self, ctx):
         if ctx.author.voice is None:
-            await ctx.send("Please join a voice channel")
+            await ctx.send('Please join a voice channel')
         else:
             voice_channel = ctx.author.voice.channel
             if ctx.voice_client is None:
@@ -39,7 +38,6 @@ class Music(commands.Cog):
     @commands.command()
     async def disconnect(self, ctx):
         # Resetting bot's state
-        self.idle = True
         self.looping = False
         self.current_track = None
         self.queue = deque()
@@ -56,12 +54,14 @@ class Music(commands.Cog):
             track = {
                 'title': info['title'],
                 'url': info['formats'][0]['url'],
-                'duration': f'{int(info["duration"] / 60)}:{info["duration"] % 60}',
+                'duration': str(datetime.timedelta(seconds=info['duration'])),
                 'thumbnail': info['thumbnails'][0]['url'],
             }
+
             self.queue.append(track)
 
-            if self.idle:
+            # Play track immediately if it is the only element
+            if len(self.queue) == 1:
                 await self.play_next(ctx)
             else:
                 await ctx.send(f'Queued {track["title"]}')
@@ -71,30 +71,26 @@ class Music(commands.Cog):
         if not self.looping:
             self.queue.popleft()
         
-        await self.play_next(ctx)
+        # Continue onto next track if it exists
+        if len(self.queue) > 0:
+            await self.play_next(ctx)
 
     async def play_next(self, ctx):
-        if len(self.queue) > 0:
-            self.idle = False
+        self.current_track = self.queue[0]
 
-            self.current_track = self.queue[0]
+        # Fetching Event Loop to create a new task i.e. to play the next song
+        # Courtesy of 
+        # https://stackoverflow.com/questions/69786149/pass-a-async-function-as-a-callback-parameter
+        el = asyncio.get_event_loop()
 
-            # Fetching Event Loop to create a new task i.e. to play the next song
-            # Courtesy of 
-            # https://stackoverflow.com/questions/69786149/pass-a-async-function-as-a-callback-parameter
-            el = asyncio.get_event_loop()
+        source = await discord.FFmpegOpusAudio.from_probe(self.current_track['url'], **FFMPEG_OPTIONS)
 
-            source = await discord.FFmpegOpusAudio.from_probe(self.current_track['url'], **FFMPEG_OPTIONS)
+        ctx.voice_client.play(
+            source,
+            after=lambda error : el.create_task(self.on_track_complete(ctx))
+        )
 
-            ctx.voice_client.play(
-                source,
-                after=lambda error : el.create_task(self.on_track_complete(ctx))
-            )
-
-            await ctx.send(f'Now playing: {self.current_track["title"]}')
-        else:
-            self.idle = True
-            await ctx.send('Reached the end of the queue')
+        await ctx.send(f'Now playing: {self.current_track["title"]}')
 
     @play.error
     async def play_error(self, ctx, error):
@@ -107,8 +103,34 @@ class Music(commands.Cog):
 
     @commands.command(aliases=['queue'])
     async def show_queue(self, ctx):
-        tracks = '\n'.join([f'{i + 1}. {track["title"]}' for i, track in enumerate(self.queue)])
+        tracks = '\n'.join(
+            [f'{"[ON LOOP] " if self.looping and i == 0 else ""}{i + 1}. {track["title"]}' for i, track in enumerate(self.queue)]
+        )
         await ctx.send(tracks)
+
+    @commands.command()
+    async def skip(self, ctx):
+        # Stops the player. Since a callback has already been registered for the current track, there is no need
+        # to do anything else. The queue will continue playing as expected.
+        ctx.voice_client.stop()
+
+        # Turn off looping
+        self.looping = False
+
+    @commands.command()
+    async def remove(self, ctx, index):
+        index = int(index) - 1
+
+        if 0 <= index and index < len(self.queue):
+            track = self.queue[index]
+            if index == 0:
+                # Invoking skip will stop the song and remove it from the queue
+                await self.skip(ctx)
+            else:
+                del self.queue[index]
+            await ctx.send(f'Removed {track["title"]}')
+        else:
+            await ctx.send(f'There is no track with that index')
 
     @commands.command()
     async def pause(self, ctx):
