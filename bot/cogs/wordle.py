465e9b0344dead835013c5fea44c1fb69f24bbfe
Trev241 <trevisliu@gmail.com>
2023-05-12 23:00:08 +0530

    Remove unnecessary kwarg

--- bot/cogs/wordle.py
+++ bot/cogs/wordle.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 from matplotlib import font_manager
 from discord.ext import commands
 
+
 class Wordle(commands.Cog):
     LETTER_SIZE = 50
     HIDDEN_WORD_IMAGE_FILENAME = 'yordle_word.png'
@@ -16,19 +17,21 @@ class Wordle(commands.Cog):
 
         # Updating champion data
         try:
-            # Fetch latest version 
+            # Fetch latest version
             version_url = 'https://ddragon.leagueoflegends.com/realms/euw.json'
-            version_data = requests.get(version_url).json(encoding='utf-8')
+            version_data = requests.get(version_url).json()
             version = version_data['v']
 
             # Fetch champion data
             champion_data_url = f'http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json'
-            response = requests.get(champion_data_url).json(encoding='utf-8')
+            response = requests.get(champion_data_url).json()
             champion_data = response['data']
 
-            self.word_bank = [champion.upper() for champion in champion_data.keys()]
+            self.word_bank = [champion.upper()
+                              for champion in champion_data.keys()]
         except Exception as e:
-            print(f'An error occured while trying to update champion data: {e}')
+            print(
+                f'An error occured while trying to update champion data: {e}')
 
     @commands.command()
     async def yordle(self, ctx):
@@ -46,19 +49,20 @@ class Wordle(commands.Cog):
 
     def image_for_hidden_word(self):
         padding = 5
-        image = Image.new(mode='RGBA', size=(Wordle.LETTER_SIZE * self.size, Wordle.LETTER_SIZE), color=(0, 0, 0, 0))
-        
+        image = Image.new(mode='RGBA', size=(
+            Wordle.LETTER_SIZE * self.size, Wordle.LETTER_SIZE), color=(0, 0, 0, 0))
+
         font = font_manager.FontProperties(family='monospace')
         file = font_manager.findfont(font)
         font = ImageFont.truetype(file, 45)
         draw = ImageDraw.Draw(image)
-        
+
         letters = {}
         for c in self.word:
             letters[c] = letters.get(c, 0) + 1
 
         position = [5, 0]
-        
+
         for i, c in enumerate(self.last_guess):
             if letters.get(c, 0):
                 letters[c] -= 1
@@ -67,9 +71,10 @@ class Wordle(commands.Cog):
             else:
                 bg = (0, 0, 0, 0)
                 fg = (180, 180, 180, 200)
-            
+
             left, top, right, bottom = draw.textbbox(position, c, font=font)
-            draw.rectangle((left - padding, top - padding, right + padding, bottom + padding), fill=bg)
+            draw.rectangle((left - padding, top - padding,
+                           right + padding, bottom + padding), fill=bg)
             draw.text(position, c, font=font, fill=fg)
 
             position[0] += Wordle.LETTER_SIZE
@@ -79,7 +84,7 @@ class Wordle(commands.Cog):
     @commands.command()
     async def guess(self, ctx, guess: str):
         guess = guess.upper()
-        
+
         if self.running and len(guess) == self.size and guess in self.word_bank:
             self.last_guess = guess
             await self.display(ctx)
@@ -87,6 +92,7 @@ class Wordle(commands.Cog):
             if guess == self.word:
                 await ctx.send(f'{ctx.author.display_name} guessed the name!')
                 self.running = False
+
 
 async def setup(client):
     await client.add_cog(Wordle(client))
