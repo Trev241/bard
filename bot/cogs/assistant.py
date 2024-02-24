7f3949b894e9c969776cb5af256aefe1f7fdb103
Trevis Liu <trevisliu@gmail.com>
2024-02-23 21:58:03 -0500

    Add speech to intent support

--- bot/cogs/assistant.py
+++ bot/cogs/assistant.py
@@ -1,6 +1,4 @@
-import re
 import os
-import asyncio
 import speech_recognition as sr
 import pvporcupine
 import numpy as np
@@ -8,29 +6,75 @@ import resampy
 import logging
 import pyttsx3
 import discord
+import audioop
+import array
+import time
+import pvrhino
+import asyncio
 
 from discord.ext import commands, voice_recv
+from collections import defaultdict
 
-logger = logging.getLogger()
+log = logging.getLogger()
 
 
 class Assistant(commands.Cog):
     def __init__(self, client):
+        # Bot state
         self.client = client
         self.recognizer = sr.Recognizer()
         self.ctx = None
         self.is_transcribing = False
         self.vc = None
+        self.is_awake = False
+        self._resampled_stream = defaultdict(lambda: list())
+        self._stream_data = defaultdict(
+            lambda: {"stopper": None, "buffer": array.array("B")}
+        )
 
-        self.wake_word_buffer = []
+        # Porcupine wake-word
+        self.speaker = None
         self.porcupine = pvporcupine.create(
             access_key=os.getenv("PV_ACCESS_KEY"),
             keyword_paths=["assistant/Okay-Bard_en_windows_v3_0_0.ppn"],
             # keywords=["picovoice", "bumblebee"],
         )
+
+        # TTS
         self.tts_engine = pyttsx3.init()
         tts_voice_id = self.tts_engine.getProperty("voices")[1].id
         self.tts_engine.setProperty("voice", tts_voice_id)
+
+        # Rhino speech-to-intent
+        self.last_frame_time = None
+        self.rhino = pvrhino.create(
+            access_key=os.getenv("PV_ACCESS_KEY"),
+            context_path="assistant/Bard-Assistant_en_windows_v3_0_0.rhn",
+            require_endpoint=False,  # Rhino will not require an chunk of silence at the end
+        )
+
+    async def detect_silence(self):
+        while self.is_awake:
+            await asyncio.sleep(0.1)
+            curr_time = time.time_ns() // 1_000_000
+
+            # Check if 500ms of silence has elapsed
+            if curr_time - self.last_frame_time > 500:
+                # Rhino will only infer intent after a chunk of silence
+                silence_frame = [0 for _ in range(512)]
+                self.detect_intent(silence_frame)
+
+                return
+
+    def detect_intent(self, audio_frame):
+        # Determine intent from speech
+        is_finalized = self.rhino.process(audio_frame)
+        if is_finalized:
+            inference = self.rhino.get_inference()
+
+            if inference.is_understood:
+                self.is_awake = False
+                log.info(inference.intent)
 
     @commands.command()
     async def wake(self, ctx):
@@ -42,6 +86,8 @@ class Assistant(commands.Cog):
         vc = await ctx.author.voice.channel.connect(cls=voice_recv.VoiceRecvClient)
 
         def callback(user: discord.User, data: voice_recv.VoiceData):
+            # log.info(f"Got packet from {user.display_name}")
+
             """
             Porcupine expects a frame of 512 samples.
 
@@ -71,90 +117,110 @@ class Assistant(commands.Cog):
             value_matrix = np.array((values[::2], values[1::2]))
 
             # Downsample the audio stream from 48kHz to 16kHz
-            resampled_values = resampy.resample(value_matrix, 48000, 16000).astype(
+            resampled_values = resampy.resample(value_matrix, 48_000, 16_000).astype(
                 value_matrix.dtype
             )
 
             # Extend the buffer with the samples collected at this instance
-            self.wake_word_buffer.extend(resampled_values[0])
+            # and choose left channel only
+            self._resampled_stream[user.id].extend(resampled_values[0])
+            resampled_buffer = self._resampled_stream[user.id]
 
-            # Allow porcupine to process the audio if sufficient samples are
-            # available
-            if len(self.wake_word_buffer) >= 512:
-                result = self.porcupine.process(self.wake_word_buffer[:512])
-                logger.info(f"Wake word result: {result}")
-                self.wake_word_buffer = self.wake_word_buffer[512:]
+            # Mark the timestamp of this audio frame as the most recent one received
+            self.last_frame_time = time.time_ns() // 1_000_000
 
-                if result == 0:
-                    vc.play(reply)
+            if len(resampled_buffer) >= 512:
+                # Buffer has >512 bytes
+                audio_frame = resampled_buffer[:512]
+                self._resampled_stream[user.id] = resampled_buffer[512:]
 
-        vc.listen(voice_recv.BasicSink(callback))
-        # vc.listen(PorcupineSink())
+                if self.is_awake:
+                    # Determine intent from speech
+                    self.detect_intent(audio_frame)
+                else:
+                    # Listen for wake word
+                    result = self.porcupine.process(audio_frame)
 
-    @commands.command()
-    async def listen(self, ctx):
-        self.ctx = ctx
-        if not self.vc:
-            self.vc = await ctx.author.voice.channel.connect(
-                cls=voice_recv.VoiceRecvClient
+                    if result == 0:
+                        # Set awake
+                        self.is_awake = True
+                        self.speaker = user
+                        log.info("Detected wake word")
+
+                        # el = asyncio.get_event_loop()
+                        # el.create_task(self.detect_silence())
+
+                        vc.play(reply)
+
+            # elif self.speaker != None and self.speaker.id == user.id:
+            #     sdata = self._stream_data[user.id]
+            #     sdata["buffer"].extend(data.pcm)
+
+            #     if not sdata["stopper"]:
+            #         sdata["stopper"] = self.recognizer.listen_in_background(
+            #             DiscordSRAudioSource(sdata["buffer"]),
+            #             self.get_bg_listener_callback(user),
+            #             phrase_time_limit=10,
+            #         )
+
+        assistant_sink = voice_recv.SilenceGeneratorSink(voice_recv.BasicSink(callback))
+        vc.listen(assistant_sink)
+
+    def get_bg_listener_callback(self, user: discord.User):
+        def callback(recognizer: sr.Recognizer, audio):
+            output = recognizer.recognize_whisper(
+                audio, model="tiny", language="english"
             )
-        self.vc.listen(voice_recv.WaveSink(f"sounds/incoming.wav"))
+            print(f'{user.display_name} said "{output}"')
 
-    @commands.command()
-    async def transcribe(self, ctx):
-        """
-        Transcribes the most recent audio instruction recorded
-        """
-        # Stop listening immediately
-        self.vc.stop_listening()
+        return callback
 
-        self.is_transcribing = True
-        message = await ctx.send(".")
-        el = asyncio.get_event_loop()
-        loading_message_task = el.create_task(self.send_loading_message(message))
 
-        audio = sr.AudioFile("sounds/incoming.wav")
-        with audio as source:
-            audio = self.recognizer.record(source)
-            loop = asyncio.get_running_loop()
+class DiscordSRAudioSource(sr.AudioSource):
+    little_endian = True
+    SAMPLE_RATE = 48_000
+    SAMPLE_WIDTH = 2
+    CHANNELS = 2
+    CHUNK = 960
 
-            # It is absolutely necessary to transcribe the text using
-            # asyncio's run_in_executor. If called normally, the call
-            # will be blocking and will not allow the loading text to
-            # appear
-            # Reference: https://stackoverflow.com/questions/77018976/async-loading-images
+    def __init__(self, buffer: array.array[int]):
+        self.buffer = buffer
+        self._entered: bool = False
 
-            result = await loop.run_in_executor(
-                None, self.recognizer.recognize_whisper, audio
-            )
-            # result = self.recognizer.recognize_whisper(audio)
+    @property
+    def stream(self):
+        return self
 
-        # Transcription finished
-        self.is_transcribing = False
-        loading_message_task.cancel()
-        result = result.strip().lower()
-        await message.edit(content=f'"{result}"')
+    def __enter__(self):
+        if self._entered:
+            log.warning("Already entered sr audio source")
+        self._entered = True
+        return self
 
-        # Execute command if available
-        args = re.split(r"\s|\,\s", result)
-        print(args)
-        for command in self.client.commands:
-            if command.name == args[0]:
-                await ctx.invoke(
-                    self.client.get_command(command.name), query=" ".join(args[1:])
-                )
+    def __exit__(self, *exc) -> None:
+        self._entered = False
+        if any(exc):
+            log.exception("Error closing sr audio source")
 
-        # Delete audio file
-        os.remove("sounds/incoming.wav")
+    def read(self, size: int) -> bytes:
+        # TODO: make this timeout configurable
+        for _ in range(10):
+            if len(self.buffer) < size * self.CHANNELS:
+                time.sleep(0.1)
+            else:
+                break
+        else:
+            if len(self.buffer) == 0:
+                return b""
 
-    async def send_loading_message(self, message):
-        spinner_dot_count = 0
+        chunksize = size * self.CHANNELS
+        audiochunk = self.buffer[:chunksize].tobytes()
+        del self.buffer[: min(chunksize, len(audiochunk))]
+        audiochunk = audioop.tomono(audiochunk, 2, 1, 1)
+        return audiochunk
 
-        while self.is_transcribing:
-            spinner_dot_count = max(1, (spinner_dot_count + 1) % 6)
-            content = "." * spinner_dot_count
-            await message.edit(content=content)
-            await asyncio.sleep(1)
+    def close(self) -> None:
+        self.buffer.clear()
 
 
 async def setup(client):
