15a725a567fe0fbaf8122ac6e90eae7ce7aeb508
Trevis Liu <trevisliu@gmail.com>
2024-09-22 01:43:02 -0400

    Fix multiple newline endings in cookies

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -26,16 +26,20 @@ class Music(commands.Cog):
 
     def __init__(self, client):
         # Convert newline endings in the cookies file
-        # try:
-        #     with open("cookies.txt", "r") as f:
-        #         cookies_data = f.read()
+        try:
+            with open("cookies.txt", "r") as f:
+                cookies_data = f.read()
 
-        #     cookies_data = cookies_data.replace("\r\n", "\n").replace("\n", os.linesep)
+            cookies_data = (
+                cookies_data.replace("\r\n", "\n")
+                .replace("\r", "\n")
+                .replace("\n", os.linesep)
+            )
 
-        #     with open("cookies.txt", "w") as f:
-        #         f.write(cookies_data)
-        # except Exception as e:
-        #     print(f"Failed to convert newline endings in cookies: {e}")
+            with open("cookies.txt", "w", newline="") as f:
+                f.write(cookies_data)
+        except Exception as e:
+            print(f"Failed to convert newline endings in cookies: {e}")
 
         self.client = client
         self.ydl = yt_dlp.YoutubeDL(Music.YDL_OPTIONS)
